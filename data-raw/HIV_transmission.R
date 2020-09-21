ddb59ce07ea0c43ca4c8e8d3949633f2208ac8d4
MattiaGirardi1997 <69383750+MattiaGirardi1997@users.noreply.github.com>
2020-09-21 18:34:49 +0200

    U

--- data-raw/HIV_transmission.R
+++ data-raw/HIV_transmission.R
@@ -1,34 +1,38 @@
-###### change to node 1, node 2
+## install packages
+install.packages("data.table", "dplyr", "tibble")
+library("data.table")
+library("dplyr")
+library("tibble")
 
 ## input data
 HIV_transmission <- fread("data-raw/HIV_transmission.csv")
 
-## nodes 
-source <- HIV %>%
+## nodes
+source <- HIV_transmission %>%
   distinct(id1) %>%
   rename(label = id1)
 
-destination <- HIV %>%
+destination <- HIV_transmission %>%
   distinct(id2) %>%
   rename(label = id2)
 
 nodes <- full_join(source, destination)
 nodes <- nodes %>% rowid_to_column("id")
 
-## edges 
-count <- HIV %>%  
+## edges
+count <- HIV_transmission %>%
   group_by(id1, id2) %>%
   summarise(weight = n()) %>%
   ungroup()
 
-edges <- count %>% 
-  left_join(nodes, by = c("id1" = "label")) %>% 
-  rename(from = id)%>% 
-  left_join(nodes, by = c("id2" = "label")) %>% 
-  rename(to = id)
+edges <- count %>%
+  left_join(nodes, by = c("id1" = "label")) %>%
+  rename(Node1 = id)%>%
+  left_join(nodes, by = c("id2" = "label")) %>%
+  rename(Node2 = id)
 
-edges <- select(edges, from, to, weight)
+edges <- select(edges, Node1, Node2, weight)
 
 ## save as rda
-save(HIV_transmission, file = "data-host/HIV_transmission.rda")
+save(edges, file = "data-host/HIV_transmission.rda")
 
